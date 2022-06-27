638ed7884f09d2eb25386c21b7fc1237895a4444
Carrie188 <81711342+Carrie188@users.noreply.github.com>
2022-06-26 17:25:39 -0700

    change the antMatch(api/user/*. disabled the authorization)

--- src/main/java/com/example/giftradar/security/AuthTokenFilter.java
+++ src/main/java/com/example/giftradar/security/AuthTokenFilter.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -24,7 +25,7 @@ import com.example.giftradar.service.UserDetailsServiceImpl;
 //loading User details (using UserDetailsService), 
 //checking Authorizaion (using UsernamePasswordAuthenticationToken).
 //
-
+@Component
 public class AuthTokenFilter extends OncePerRequestFilter{
 	@Autowired
 	  private JwtUtils jwtUtils;
